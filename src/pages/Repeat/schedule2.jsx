6b72ffd50ecdeb6629e498a572e93533b8e1566d
DasuNadeema <dasunnadeema6@gmail.com>
2023-11-25 00:03:50 +0530

    added tv shows and footer

---
+++ src/pages/Repeat/schedule2.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../pages/Schedule.css";
+import Card from "./Card";
+
+function Schedule() {
+  return (
+    <div>
+      <section id="schedule" className="schedule">
+        <div className="container-fluid">
+          <div className="row">
+            <h4 className="section-title">Tv Shows</h4>
+          </div>
+          <div className="row"></div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Schedule;
